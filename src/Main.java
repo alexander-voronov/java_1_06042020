c425bc01d89bc0d421c8a94f7234a0e1e901a387
Alexander Voronov <dr.strum@gmail.com>
2020-04-08 21:51:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        №1-2
+        byte a = 1;
+        short b = 100;
+        int c = -1000;
+        long d = 55250l;
+        float e = 3.3f;
+        double f = 5.234;
+        char j = 0;
+        boolean h = true;
+        String i = "test";*/
+
+        /*
+        №3
+        static int formula(int a, int b, int c, int d) {
+            return a * (b + (c / d));
+        }*/
+
+
+       /*
+       №4
+       double t = 9;
+
+        if (amount(t)) {
+            System.out.println("Привозите");
+        } else {
+            System.out.println("время для меня не подходит");
+        }
+
+    }
+    static boolean amount(double t){
+        return 10 <= t || t <= 20;}*/
+
+
+       /*
+       №5
+
+        int a = -1;
+
+        if (a >= 0) {
+            System.out.println("число положительное");
+        } else {
+            System.out.println("число отрицательное");
+        }
+        */
+
+       /*
+       №6
+            if isNegative (0) {
+                b > 0;
+            }
+
+            public static boolean isNegative(int b) {
+            return true;
+        }
+        */
+
+
+    }
+
+    /*
+    №7
+    static void printWord(String name) {
+        System.out.println("Привет, " + name);
+
+    }
+    */
+
+}
