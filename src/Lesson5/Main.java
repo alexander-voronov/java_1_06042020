c15720155ac8a5c4cdbd1c064c0fc6b6141d6930
Alexander Voronov <dr.strum@gmail.com>
2020-04-21 23:37:05 +0300

    Commit_21.04.2020_HW_5

---
+++ src/Lesson5/Main.java
@@ -0,0 +1,22 @@
+package Lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        //Employee employee1 = new Employee("Иванов Иван Иванович", "manager","email@email.com", "8-495-123-45-67", 100000, 35);
+        //employee1.info();
+
+
+        Employee[] empArray = new Employee[5];
+        empArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312311", 30000, 30);
+        empArray[1] = new Employee("Petrov Petr", "Engineer", "ppetrov@mailbox.com", "892312312", 30000, 50);
+        empArray[2] = new Employee("Sidorov Sidr", "secretary", "ssidorov@mailbox.com", "892312313", 45000, 30);
+        empArray[3] = new Employee("Sapogov Kot", "Manager", "skot@mailbox.com", "892312314", 50000, 25);
+        empArray[4] = new Employee("Maslov George", "Director", "gendir@mailbox.com", "892312315", 3000000, 67);
+
+        for (int i = 0; i < empArray.length ; i++) {
+          if(empArray[i].getAge() > 40){
+                empArray[i].info();
+            }
+        }
+    }
+}
